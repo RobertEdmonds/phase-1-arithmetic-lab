6ea3321cfe122cf700a1252b81dce87b80f4b5bc
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-26 20:43:45 -0700

    commit tree f0f22d5c73d545b46782141f4d2570bbfd536b60

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
 
+let random = Math.floor(Math.random()*20)+1
+console.log(random)
+
+let num3 = 20
+let num4 = 5
+const mod = num3 / num4
+
+let max = Math.max(1,2,3,4,20)
+console.log(max)
 // Write your code here
